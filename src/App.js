5b0f23f2b6093554ef927cb4eb725e228101c851
souravrane <sourav1121997@gmail.com>
2021-11-17 22:41:53 +0530

    homepage updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import HomePage from "./homepage.component";
-import "./homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.styles.scss";
 
 function App() {
   return (
