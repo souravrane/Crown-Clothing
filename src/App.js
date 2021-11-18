66354956f1073b9533abdf2b629739b323219b27
souravrane <sourav1121997@gmail.com>
2021-11-19 01:26:11 +0530

    added router to app and updated menu item with router urls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 
 import "./App.css";
 
+const TopicList = () => {
+  return (
+    <div>
+      <h1>Topic List page</h1>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  return (
+    <div>
+      <h1>Topic Detail page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicList} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { withRouter } from "react-router";
+
 import "./menu-item.styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => {
-  return (
-    <div className={`menu-item ${size}`}>
+export const MenuItem = withRouter(
+  ({ title, imageUrl, size, history, linkUrl, match }) => {
+    return (
       <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="content">
-        <div className="title">{title.toUpperCase()}</div>
-        <span className="subtitle">SHOP NOW</span>
+        className={`menu-item ${size}`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+      >
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        />
+        <div className="content">
+          <div className="title">{title.toUpperCase()}</div>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
